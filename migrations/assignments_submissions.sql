75321309742f65fcb4c7dc833d8ced827bc86bf6
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-26 17:54:57 -0400

    create table file for assignments_submissions and assignments

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,3 +1,7 @@
+
+drop table if exists assignment_submissions;
+drop table if exists assignments;
+
 CREATE TABLE assignments (
 id SERIAL PRIMARY KEY NOT NULL,
 name VARCHAR(255) NOT NULL,
@@ -8,7 +12,7 @@ duration INTEGER
 );
 
 
-CREATE TABLE assignment_submission (
+CREATE TABLE assignment_submissions (
 id SERIAL PRIMARY KEY NOT NULL,
 assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
 student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
