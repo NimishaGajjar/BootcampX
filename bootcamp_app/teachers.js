b9cdd614d6fa0fedf0fa66e3b9649f6a2ff0edf4
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-28 21:19:31 -0400

    exercise to use node-postgre

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,26 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const args = process.argv.slice(2);
-
-/*
-  Queries
-*/
 
 const queryString = `
-SELECT students.id, students.name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-LIMIT $2;
+ORDER BY name;
 `;
 
-const values = [`%${args[0]}%`, args[1] || 5];
+const values = [`${process.argv[2] || 'JUL02'}%`];
 
 pool.query(queryString, values)
   .then(res => {
-    res.rows.forEach(student => {
-      console.log(`${student.name} has an id of ${student.id} and was in the ${student.cohort} cohort`);
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.cohort}: ${teacher.name}`);
     });
   })
   .catch(err => {
