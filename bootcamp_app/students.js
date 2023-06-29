48121acc237626a424049798a8ee10d78cb24a39
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-28 21:28:09 -0400

    exercise to use node-postgre

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,28 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const args = process.argv.slice(2);
-
-/*
-  Queries
-*/
-
 const queryString = `
-SELECT students.id, students.name, cohorts.name as cohort
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohort_id = cohorts.id
+JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE $1
 LIMIT $2;
 `;
 
-const values = [`%${args[0]}%`, args[1] || 5];
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// store all potentially malicious files in a separate array
+const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values)
   .then(res => {
-    res.rows.forEach(student => {
-      console.log(`${student.name} has an id of ${student.id} and was in the ${student.cohort} cohort`);
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     });
-  })
-  .catch(err => {
-    console.error('query error', err.stack);
-  });
+  }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,25 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
 const queryString = `
-SELECT DISTINCT teachers.name as name, cohorts.name as cohort
-FROM assistance_requests
-JOIN teachers ON teacher_id = teachers.id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE $1
-ORDER BY name;
+WHERE cohorts.name = $1
+ORDER BY teacher;
 `;
 
-const values = [`${process.argv[2] || 'JUL02'}%`];
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
 
 pool.query(queryString, values)
   .then(res => {
-    res.rows.forEach(teacher => {
-      console.log(`${teacher.cohort}: ${teacher.name}`);
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
     });
-  })
-  .catch(err => {
-    console.error('query error', err.stack);
-  });
+  }).catch(err => console.error('query error', err.stack));
